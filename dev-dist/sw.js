df40cc478711f6446f6b0d489c7cf5fa49619a42
Samuel Duque <duquepuchi@gmail.com>
2024-08-18 16:16:12 +0200

    icons

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cpl1h8cepjg"
+    "revision": "0.eb2nft4gam"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
